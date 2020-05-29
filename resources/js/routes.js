5dd5a7a8acf5d07e82ed2d85f2bda86dc64d854a
Toan Dang <ddtoan991205@gmail.com>
2020-05-29 19:47:05 +0700

    Show customer list

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,9 @@
 import Home from './components/Home.vue';
 import Login from './components/auth/Login.vue';
 
+import CustomersMain from './components/customers/Main.vue';
+import CustomersList from './components/customers/List.vue';
+// import Customer from './components/customers/View.vue';
 
 export const routes = [
   {
@@ -13,5 +16,26 @@ export const routes = [
   {
     path: '/login',
     component: Login
+  },
+  {
+    path: '/customers',
+    component: CustomersMain,
+    meta: {
+      requiresAuth: true
+    },
+    children: [
+      {
+        path: '/',
+        component: CustomersList
+      },
+      // {
+      //   path: 'new',
+      //   component: NewsCustomer
+      // },
+      // {
+      //   path: ':id',
+      //   component: Customer
+      // }
+    ]
   }
 ]
